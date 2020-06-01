899d8dfdfd670047e6533eaec76e95968d324866
kkudo020 <k.kudo@cuore.jp>
2020-06-01 11:25:36 +0900

    利用者一覧 入所情報一覧 入所管理トップ　の追加

---
+++ kaigoapp/pages/nyusyo_list.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+      <div>
+          <h1>{{title}}</h1>
+          <p>{{message}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data: function(){
+        return{
+            title:'入所情報一覧',
+            message:'入所情報を選択してください。',
+        };
+    },
+}
+</script>
+<style>
+div{
+    padding: 10pt;
+}
+a{
+    margin-top: 1em;
+}
+
+</style>
