94a187ae7795168a4f3e38a531b1c616560926e0
yamada <k.yamada@core.jp>
2020-06-02 10:46:34 +0900

    日付処理追加と管理画面

--- kaigoapp/pages/bed.vue
+++ kaigoapp/pages/bed.vue
@@ -3,73 +3,74 @@
     <h1>ベッドの空き状況</h1>
     <no-ssr>
       <v-date-picker 
-              :input-props="{ class: 'input', name: 'event_dates', placeholder:'日付を入力' }"
+              :input-props="{ class: 'input', name: 'event_dates' }"
               :mode="mode" 
               :formats="formats"
-              v-model="selectedDate" class="date">
+              :update-on-input="true"
+              v-model="selectedDate" @input="inputEvent" >
       </v-date-picker>
     </no-ssr>
     <table>
       <tr>
         <th rowspan="7">1F</th>
         <th></th>
-        <th>日付1</th>
-        <th>日付2</th>
-        <th>日付3</th>
-        <th>日付4</th>
-        <th>日付5</th>
-        <th>日付6</th>
-        <th>日付7</th>
+        <th>{{ day1 }}</th>
+        <th>{{ day2 }}</th>
+        <th>{{ day3 }}</th>
+        <th>{{ day4 }}</th>
+        <th>{{ day5 }}</th>
+        <th>{{ day6 }}</th>
+        <th>{{ day7 }}</th>
       </tr>
       <tr>
         <td>101A</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
+        <td><button><a href="/nyusyokanri_main">空</a></button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
       </tr>
       <tr>
         <td>101B</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
       </tr>
       <tr>
         <td>101C</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
       </tr>
       <tr>
         <td>101D</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
       </tr>
       <tr>
         <td>102A</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
-        <td>空</td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
+        <td><button>空</button></td>
       </tr>
     </table>
   </div>
@@ -80,19 +81,39 @@
 
 export default {
   data:function() {
+    var d = new Date();
     return {
       mode:'single',
       formats: {
         input: ['YYYY-MM-DD'],
       },
-      selectedDate:new Date(),
+      selectedDate:d,
+      day1: (d.getMonth() + 1) + '/' + d.getDate() ,
+      day2: (d.getMonth() + 1) + '/' + (d.getDate() + 1) ,
+      day3: (d.getMonth() + 1) + '/' + (d.getDate() + 2) ,
+      day4: (d.getMonth() + 1) + '/' + (d.getDate() + 3) ,
+      day5: (d.getMonth() + 1) + '/' + (d.getDate() + 4),
+      day6: (d.getMonth() + 1) + '/' + (d.getDate() + 5) ,
+      day7: (d.getMonth() + 1) + '/' + (d.getDate() + 6) ,
     }
   },
+  methods: {
+    inputEvent: function() {
+        var newday = this.selectedDate;
+        this.day1 = (newday.getMonth() + 1) + '/' + newday.getDate();
+        this.day2 = (newday.getMonth() + 1) + '/' + (newday.getDate() + 1);
+        this.day3 = (newday.getMonth() + 1) + '/' + (newday.getDate() + 2);
+        this.day4 = (newday.getMonth() + 1) + '/' + (newday.getDate() + 3);
+        this.day5 = (newday.getMonth() + 1) + '/' + (newday.getDate() + 4);
+        this.day6 = (newday.getMonth() + 1) + '/' + (newday.getDate() + 5);
+        this.day7 = (newday.getMonth() + 1) + '/' + (newday.getDate() + 6);
+      } 
+  }
 }
 </script>
 
 <style>
-.date {
+input {
   margin-left: 1em;
 }
 table {
@@ -102,5 +123,12 @@ table {
 }
 th, td {
   border: solid 1px black;
+}
+td {
+  text-align: center;
+}
+button {
+  background-color: white;
+  border: solid 1px white;
 }
 </style>
