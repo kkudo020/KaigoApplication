0618404a93f99240e81c24e9a3a83a076ae696ab
kkudo020 <k.kudo@cuore.jp>
2020-06-03 11:31:32 +0900

    新規作成ボタンの追加

--- kaigoapp/pages/nyusyo_list.vue
+++ kaigoapp/pages/nyusyo_list.vue
@@ -6,6 +6,7 @@
           <p>{{message}}</p>
       </div>
       <p>[{{personal_data}}] 様</p>
+      <button style="width:7em; margin-top:5pt;" @click="newcreate()">新規作成</button>
           <table class="list">
               <tr>
                   <th>フロア</th>
@@ -14,12 +15,12 @@
                   <th>終了日</th>
                   <th>備考</th>
               </tr>
-              <tr v-for="data in stay_data" :key="data" @click="doAction();" >
+              <tr v-for="(data,key) in stay_data" :key="key" @click="doAction();" >
                 <td style="text-align:center" width="15%">{{bed_data[data.BED_ID]["FLOOR"]}}</td>
                 <td style="text-align:center" width="15%">{{bed_data[data.BED_ID]["BED_NUM"]}}</td>
                 <td width="20%">{{data.START_DAY}}</td>
                 <td width="20%">{{data.LAST_DAY}}</td>
-                <td width="30%">{{data.BIRTHDAY}}</td>
+                <td width="30%">ID：{{key}}</td><!--デバッグ用仮置き-->
               </tr>
           </table>
   </div>
@@ -54,7 +55,38 @@ export default {
         doAction(){
             // this.$router.push('nyusyokanri_main');
             window.location.href = '/nyusyokanri_main';
+        },
+        newcreate(){
+            console.log('新規作成');
         }
     }
 }
 </script>
+<style>
+.list{
+  border-collapse: collapse;
+  margin: 10pt 0;
+  width: 50%;
+  height: 3em;
+}
+.list th{
+    font-size: 14pt;
+    color: white;
+    padding: 5pt;
+    border: 2px solid gray;
+    background: steelblue;
+}
+.list tr{
+    padding: 1pt 5pt;
+    border: 2px solid gray;
+    background: rgb(223, 234, 238);
+}
+.list tr:hover{
+    background: rgb(158, 215, 236);
+    cursor: pointer;
+}
+.list td{
+    padding: 1pt 5pt;
+    border: none;
+}
+</style>
