bd97e3a3c0734909e63e98417bfb500d14e25329
kkudo020 <k.kudo@cuore.jp>
2020-06-04 15:39:27 +0900

    bed.vueカレンダーへの入所情報の表示

--- kaigoapp/pages/bed.vue
+++ kaigoapp/pages/bed.vue
@@ -20,9 +20,9 @@
         <th rowspan="8" v-if="data.BED_ID == flag">{{ data.FLOOR }}F</th>
         <td class="room">{{ data.BED_NUM }}</td>
         <td v-for="(c_data,c_key) in day" v-bind:key="c_key">
-          <!-- <button v-if="data.SCHEDULE.hasOwnProperty((c_data.getMonth()+1)+'_'+c_data.getDate())" onclick="#">{{data}}</button> -->
-          <!--data.SCHEDULE[(c_data.getMonth()+1)+"_"+c_data.getDate()]-->
-          <button class="button" @click="blank(data.BED_ID,c_data);">空</button><!--クリックでメソッド(data.BED_ID,date)へ-->
+          <button v-if="data.SCHEDULE[(c_data.getMonth()+1)+'_'+c_data.getDate()]" @click="stayed(data.SCHEDULE[(c_data.getMonth()+1)+'_'+c_data.getDate()].stay_id);">{{staySwitch(data.SCHEDULE[(c_data.getMonth()+1)+'_'+c_data.getDate()].stay_id)}}</button>
+          <!--data.SCHEDULE[(c_data.getMonth()+1)+'_'+c_data.getDate()].stay_id-->
+          <button v-else class="button" @click="blank(data.BED_ID,c_data);">空</button><!--クリックでメソッド(data.BED_ID,date)へ-->
         </td>
       </tr>
     </table>
@@ -31,7 +31,7 @@
 
 <script>
 const axios = require('axios');
-let url = "https://kaigo-db-a268b.firebaseio.com/BED.json";
+let url = "https://kaigo-db-a268b.firebaseio.com/";
 
 
 export default {
@@ -52,23 +52,34 @@ export default {
   methods: {
     inputEvent: function() {
         var newday = new Date(this.selectedDate);
-        console.log(newday);
         for(var i=0;i<7;i++){
           this.$set(this.day,i,new Date(newday));
           //(newday.getMonth() + 1) + '/' + newday.getDate() +'('+ this.weekDay[newday.getDay()] +')'
           newday.setDate(newday.getDate()+1);
         }
-        console.log(this.day);
       },
     blank: function(bedid,date){
       this.$store.commit('set_bedid',bedid);
       this.$store.commit('set_date',date);
       this.$router.push('user_list');
+    },
+    stayed: function(stay_id){
+      this.$store.commit('set_stayid',stay_id);
+      this.$router.push('/nyusyokanri_main');
+    },
+    staySwitch: function(stay_id){
+      axios.get(url+"STAY/"+stay_id+".json").then((res)=>{
+        console.log(url+"STAY/"+stay_id+".json");
+        let p_id = res.data.PERSONAL_ID;
+        axios.get(url+"PERSONAL/"+p_id+".json").then((p_res)=>{
+          return (p_res.data.P_NAME);
+        });
+      });
     }
   },
   asyncData: async function() {
-    let result = await axios.get(url);
-    return { room_data: result.data };
+    let res = await axios.get(url+"BED.json");
+    return { room_data: res.data };
   },
 }
 </script>
@@ -86,6 +97,7 @@ export default {
 }
 .table th, td {
   border: solid 1px black;
+  width: 8em;
 }
 .table th {
   background-color: gray;
